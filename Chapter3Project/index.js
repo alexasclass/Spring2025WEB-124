125582d11e72e1f48072190d3a78286ea8236f8c
alexasclass <lmarti20@stumail.jccc.edu>
2025-04-05 19:33:36 -0500

    Add files via upload

---
+++ Chapter3Project/index.js
@@ -0,0 +1,35 @@
+// My full name:
+var fullName = "Leticia Martinez";
+console.log("My full Name:", fullName);
+
+
+// This section is my desire  annual salary.
+var desiredAnnualSalary = 120000;
+console.log("Desired Annual Salary", desiredAnnualSalary);
+
+// I am not a veterean so I say false>
+    var isVeteran = false; 
+console.log("Veteran Status:", isVeteran);
+
+//My tree friends names goes here
+var friends = [
+ "Alem", 
+"Rocio",
+ "Isela"
+];
+console.log("Friends:", friends);
+
+// My friends salary
+const MyfriendsSalaries = [
+    "Alem=50000", 
+    "Rocia=65000", 
+    "Isela=100000"
+];
+console.log("My friends Salaries:", MyfriendsSalaries);
+
+const anotherFriend = {
+    firstName: "Alma",
+    lastName: "Aguero",
+    desiredAnnualSalary: 150000
+};
+console.log("different friend:", anotherFriend);
